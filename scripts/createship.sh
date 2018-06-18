c27148e7c7986a67f5a36050f132a0a5e556a3d5
assaf <asssaf@localhost>
2018-06-17 17:19:37 -0700

    specify exact tag in scripts

--- scripts/createship.sh
+++ scripts/createship.sh
@@ -8,10 +8,10 @@ fi
 
 SHIP="$1"
 CODE="$2"
-IMAGE="asssaf/urbit"
+: ${URBIT_IMAGE:="asssaf/urbit:0.6.0-debian"}
 
 docker run -ti --rm \
 	--net host \
 	-v $(pwd):/urbit \
 	--user $(id -u):$(id -g) \
-	${IMAGE} -w ${SHIP} -t ${CODE}
+	${URBIT_IMAGE} -w ${SHIP} -t ${CODE}
