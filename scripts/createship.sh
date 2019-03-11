20d58d1d967188e1ac9c8e78b5698e770f4232f2
assaf <asssaf@localhost>
2019-03-10 17:17:59 -0700

    use key file instead of passing the key on the command line

--- scripts/createship.sh
+++ scripts/createship.sh
@@ -2,16 +2,25 @@
 
 if [ $# -ne 2 ]
 then
-	>&2 echo "Usage: $(basename $0) <ship> <key>"
+	>&2 echo "Usage: $(basename $0) <ship> <keyfile>"
 	exit 1
 fi
 
 SHIP="$1"
-KEY="$2"
+KEY_FILE="$2"
 : ${URBIT_IMAGE:="asssaf/urbit:0.7.3-debian"}
+
+if [ ! -f "$KEY_FILE" ]
+then
+	>&2 echo "Key file is not readable: '$KEY_FILE'"
+	exit 2
+fi
+
+ABS_KEY_FILE="$(readlink -e $KEY_FILE)"
 
 docker run -ti --rm \
 	--net host \
 	-v $(pwd):/urbit \
+        -v "$ABS_KEY_FILE:/mnt/key_file:ro" \
 	--user $(id -u):$(id -g) \
-	${URBIT_IMAGE} -w ${SHIP} -G ${KEY}
+	${URBIT_IMAGE} -w ${SHIP} -k /mnt/key_file
